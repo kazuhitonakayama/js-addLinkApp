8598c20c2b10ab1522581e3b736960d3a173d1e8
kazuhitonakayama <firstjin19990101@gmail.com>
2020-06-25 12:47:03 +0900

    Fixing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
-module ApplicationHelper
+module ApplicationHelper3
+      require "uri"
+     
+      def text_url_to_link(text)
+     
+        URI.extract(text, ["http", "https"]).uniq.each do |url|
+          text.gsub!(url, "#{url}")
+        end
+        text
+      end
 end
